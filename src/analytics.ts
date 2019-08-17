344792004414e8b110912b17d2778283a0a74c06
E. Weilow <eweilow@digitalkoppar.se>
2019-08-17 10:40:21 +0200

    Update analytics.ts

--- src/analytics.ts
+++ src/analytics.ts
@@ -1,10 +1,11 @@
+/*
 import "autotrack/lib/plugins/outbound-form-tracker";
 import "autotrack/lib/plugins/outbound-link-tracker";
 import "autotrack/lib/plugins/url-change-tracker";
 
 (window as any).dataLayer = (window as any).dataLayer || [];
 function gtag() {
-  /* eslint prefer-rest-params: 0 */
+  // eslint prefer-rest-params: 0
   (window as any).dataLayer.push(arguments);
 }
 (gtag as any)("js", new Date());
@@ -14,3 +15,4 @@ function gtag() {
 (gtag as any)("require", "urlChangeTracker");
 // eslint-disable-next-line @typescript-eslint/camelcase
 (gtag as any)("config", "UA-113469156-1", { anonymize_ip: true });
+*/
